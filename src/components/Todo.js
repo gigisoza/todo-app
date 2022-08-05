5253fb34c94a57b69d00265c9004b4ea067e89d7
gigisoza <sozashviligigi@gmail.com>
2022-08-06 03:38:52 +0400

    First commit

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+export default function Todo() {
+    const [todo, setTodo] = useState("");
+
+    const handleChange =(event)=>{
+        setTodo(event.target.value);
+    };
+    return (<div>
+        <form>
+            <input value={todo} onChange={handleChange} type="text"></input>
+            <button type="submit">Add</button>
+        </form>
+    </div>);
+}

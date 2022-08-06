004f9496efc29a308d25848a38ede863957cddf0
gigisoza <sozashviligigi@gmail.com>
2022-08-06 05:57:52 +0400

    delete useless files add From, Header components and style module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Todo from "./components/Todo";
-import "./App.css";
-
+import Header from "./components/Header";
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
-      <Todo></Todo>
+      <Header></Header>;
+      <Form></Form>;
     </div>
   );
 }
--- src/components/ListItem.js
+++
@@ -1,3 +0,0 @@
-export default function ListItem({name}) {
-    return <div>{name}</div>;
-}

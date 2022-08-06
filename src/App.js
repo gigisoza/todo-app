839de0280d41073a37af44723f144867ff996d3f
gigisoza <sozashviligigi@gmail.com>
2022-08-06 06:35:59 +0400

    added Todo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import { useState } from "react";
+import Todo from "./components/Todo";
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
       <Header></Header>;
-      <Form></Form>;
+      <Form todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList}></Form>
+      <Todo/>
     </div>
   );
 }
